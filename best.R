5220f1fae2b9e01de89e002e54407a72e44cdfb4
habibiDC <47903921+habibiDC@users.noreply.github.com>
2019-02-28 23:47:42 -0500

    Update best.R

--- best.R
+++ best.R
@@ -1,7 +1,8 @@
 best<-function(state, outcome){
+        #read outcome data
         data<-read.csv('/Users/zeinameng/R/rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv', colClasses = 'character')
-        #check that state and outcome are valid
-        if (state %in% state.abb && outcome %in% c('heart attack', 'heart failure','pneumonia')){
+        #check that state, outcome are valid
+        if (state %in% data$State && outcome %in% c('heart attack', 'heart failure','pneumonia')){
                 if (outcome=='heart attack'){
                         column=11
                 }
@@ -11,9 +12,11 @@ best<-function(state, outcome){
                 if (outcome=='pneumonia'){
                         column=23
                 }
-                #read outcome data
+                #read outcome data with specified state and outcome and return hospital name column too
                 outcome<-data[data$State==state, c(2,column)]
+                #convert outcome column data to numeric becaue it was imported as character
                 outcome[,2]<-as.numeric(outcome[,2])
+                #remove missing data in the outcome column
                 newdata<-outcome[complete.cases(outcome[,2]),]
                 sort1<-newdata[order(newdata[,2],newdata[,1]), ]
                 #return hospital name
@@ -22,12 +25,8 @@ best<-function(state, outcome){
         #error message for invalid arguments
         else{
         if (state %in% state.abb){
-        stop('invalid outcome')}
-                else{
-                if(outcome %in% c('heart attack', 'heart failure','pneumonia')){
-                stop('invalid state')}
-                      else{stop('invalid state and outcome')}          
-                     }
+                stop('invalid outcome')}
+                        else {stop('invalid state')}          
         }              
 }
         
